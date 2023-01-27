01c4015337f1623271b75f2eca4039d246294583
Daniel <danielreyesaleman4@unitec.edu>
2023-01-27 13:38:43 -0600

    Menu del archivo

---
+++ src/lab2p2_danielreyes/Lab2P2_DanielReyes.java
@@ -0,0 +1,15 @@
+
+package lab2p2_danielreyes;
+
+
+public class Lab2P2_DanielReyes {
+
+    
+    public static void main(String[] args) {
+        int resp = 0;
+        while(resp != 4){
+            
+        }
+    }
+    
+}
